ea601ad77cc9959fffff8fcc5880368eaa5fde2c
Daniel Windsor <dw.accounts@protonmail.com>
2020-02-21 10:32:20 +1300

    Add listener

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("connected")
+// const keys = Array.from(document.querySelectorAll('.key'));
+
+window.addEventListener('keydown', keyPress);
+
+
+function keyPress(e) {
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+
+  if(!key) {return}
+
+  key.classList.add('pressed')
+}
