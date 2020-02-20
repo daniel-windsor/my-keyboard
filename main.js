4e28f628ea2e538473ab846066b604a65424c1cb
Daniel Windsor <dw.accounts@protonmail.com>
2020-02-21 11:17:19 +1300

    Play with colours

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-// const keys = Array.from(document.querySelectorAll('.key'));
-
-window.addEventListener('keydown', keyPress);
-
-
 function keyPress(e) {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
 
-  if(!key) {return}
+  if (!key) {
+    return
+  }
 
   key.classList.add('pressed')
 }
+
+function endTransition(evt) {
+  evt.target.classList.remove('pressed')
+}
+
+window.addEventListener('keydown', keyPress);
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', endTransition));
